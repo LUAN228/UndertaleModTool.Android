6938e50e1fa4300d1177adfa2fd12b7ff1d47a4b
LUAN228 <luanfghsilverio@gmail.com>
2026-07-31 23:19:38 -0300

    Add initial App class for Android application

---
+++ UndertaleModTool.Android/App.axaml.cs
@@ -0,0 +1,23 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+
+namespace UndertaleModTool.Android;
+
+public partial class App : Application
+{
+    public override void Initialize()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        if (ApplicationLifetime is ISingleViewApplicationLifetime singleView)
+        {
+            // Ponto de entrada da interface do Android
+        }
+
+        base.OnFrameworkInitializationCompleted();
+    }
+}
