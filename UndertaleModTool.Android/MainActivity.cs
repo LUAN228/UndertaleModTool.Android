3585ba10cb69653712bfc476c6ac46f38695dba5
LUAN228 <luanfghsilverio@gmail.com>
2026-07-31 22:47:31 -0300

    fix: remover referência ao tema AppCompat inexistente

---
+++ UndertaleModTool.Android/MainActivity.cs
@@ -0,0 +1,19 @@
+using Android.App;
+using Android.Content.PM;
+using Avalonia;
+using Avalonia.Android;
+
+namespace UndertaleModTool.Android;
+
+[Activity(
+    Label = "@string/app_name",
+    MainLauncher = true,
+    ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
+public class MainActivity : AvaloniaMainActivity
+{
+    protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
+    {
+        return builder
+            .UseAndroid();
+    }
+}
