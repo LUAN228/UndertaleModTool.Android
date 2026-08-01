fbb0fb0257de75db461fd16a9dc2386f2e061771
LUAN228 <luanfghsilverio@gmail.com>
2026-07-31 23:34:25 -0300

    Refactor MainActivity namespace and class reference

--- UndertaleModTool.Android/MainActivity.cs
+++ UndertaleModTool.Android/MainActivity.cs
@@ -3,15 +3,14 @@ using Android.Content.PM;
 using Avalonia;
 using Avalonia.Android;
 using Avalonia.ReactiveUI;
-using UndertaleModToolAvalonia;
 
-namespace UndertaleModToolAvalonia.Android;
+namespace UndertaleModTool.Android;
 
 [Activity(
     Label = "@string/app_name",
     MainLauncher = true,
     ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
-public class MainActivity : AvaloniaMainActivity<App>
+public class MainActivity : AvaloniaMainActivity<UndertaleModTool.Android.App>
 {
     protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
     {
